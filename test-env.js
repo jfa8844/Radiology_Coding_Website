b56aa52ed93b62b8cb847d7354bba12afbeb77f2
jfa8844 <Jfa8844@gmail.com>
2025-09-02 16:14:16 +0000

    repaired .env workflow

--- test-env.js
+++
@@ -1,7 +0,0 @@
-// test-env.js
-require('dotenv').config();
-
-console.log('--- TEST SCRIPT ---');
-console.log('URL from env is:', process.env.SUPABASE_URL);
-console.log('KEY from env is:', process.env.SUPABASE_ANON_KEY);
-console.log('-------------------');
